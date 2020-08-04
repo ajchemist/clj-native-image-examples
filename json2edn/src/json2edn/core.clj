dc7d5723a504fd5b1cb51299237c60745dc9c32c
ajchemist <1694505+aJchemist@users.noreply.github.com>
2020-08-04 15:27:56 +0900

    Added json2edn example

---
+++ json2edn/src/json2edn/core.clj
@@ -0,0 +1,13 @@
+(ns json2edn.core
+  "Original source code: https://gist.github.com/taylorwood/23d370f70b8b09dbf6d31cd4f27d31ff"
+  (:require
+   [clojure.data.json :as json]
+   [clojure.edn :as edn])
+  (:gen-class))
+
+
+(defn -main
+  [& args]
+  (if (string? (first args))
+    (prn (json/read-str (slurp (first args)) :key-fn keyword))
+    (prn (json/read *in* :key-fn keyword))))
