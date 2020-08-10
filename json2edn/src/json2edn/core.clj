70a10a3e150e263218c0c62ad63278f2a78ec48d
ajchemist <1694505+aJchemist@users.noreply.github.com>
2020-08-10 14:05:49 +0900

    Added ci skip condition -> workflow

--- json2edn/src/json2edn/core.clj
+++ json2edn/src/json2edn/core.clj
@@ -2,7 +2,7 @@
   "Original source code: https://gist.github.com/taylorwood/23d370f70b8b09dbf6d31cd4f27d31ff"
   (:require
    [clojure.data.json :as json]
-   [clojure.edn :as edn])
+   )
   (:gen-class))
 
 
